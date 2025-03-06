60048922285d8bce9f807ec6766d9b6cfbf0565b
Kjetil Fjellheim <kjetil@forgottendonkey.net>
2025-03-06 19:17:12 +0100

    Revert "feat: editable-http-headers (#9)"
    This reverts commit 87598d4897bdafd5775246314916216fa4554976.

--- apinae-daemon/tests/test_http_mock.rs
+++ apinae-daemon/tests/test_http_mock.rs
@@ -9,7 +9,6 @@ mod common;
 #[tokio::test(flavor = "multi_thread", worker_threads = 10)]
 async fn test_http_server() {
     // Start the server.
-    #![allow(clippy::zombie_processes)]
     let mut server_command = common::start_server("./tests/resources/test_http_mock.json", "1")
         .await
         .expect("Failed to start server");
